d456369e13de98aa937995dc14a51cb01cecf836
Jimin Kim <0305jimin@gmail.com>
2025-02-21 19:05:49 +0900

    all: removing comments and adding section titles

--- calibration.py
+++ calibration.py
@@ -7,13 +7,11 @@ criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 W = 23
 L = 31
 
-# prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
 objp = np.zeros((W*L, 3), np.float32)
 objp[:,:2] = np.mgrid[0:L,0:W].T.reshape(-1,2)
 
-# Arrays to store object points and image points from all the images.
-objpoints = [] # 3d point in real world space
-imgpoints = [] # 2d points in image plane.
+objpoints = [] 
+imgpoints = [] 
 
 imgname = 'camera_calib5.jpg'
 imgpath = f'images/{imgname}'
--- video_recording.py
+++ video_recording.py
@@ -14,27 +14,14 @@ print(sys.argv[1])
 camera_name = sys.argv[1]
 
 gstreamer_str = f"icamerasrc device-name=isx031-{camera_name} ! video/x-raw,format=UYVY,width=1920,height=1080,framerate=30/1 ! videoconvert ! appsink"
-            # ximagesource"
-            # ximagesink: video frame into XWindow local/remote display    
-# # gst-launch-1.0: build and run GStreamer pipeline. 
-# icamerasrc: option for source file and in this case, it's for icamera, use 'icamersrc'
-# video/x-raw: unstructured and uncompressed raw video data. else: x-ffv, x-h263, x-huffyuv .... etc.     
-# # print-fps : ** couldn't find that where the value is printed. -> deleted      
-# video convert : one of GstVideo Convert Scale (video convert, video convert scale, video scale)
 
-# cap = cv.VideoCapture(1) -> [ WARN:0@982.602] global cap_v4l.cpp:913 open VIDEOIO(V4L2:/dev/video1): can't open camera by index
 cap = cv.VideoCapture(gstreamer_str, cv.CAP_GSTREAMER)
 
-
-# cap.set(5, 30.0) # FPS
-# cap.set(3, 1920) # WIDTH
-# cap.set(4, 1080) # HEIGHT
 print("FPS: ", cap.get(5))
 print(cap.isOpened())                                                                                        
 # https://docs.opencv.org/2.4/modules/highgui/doc/reading_and_writing_images_and_video.html?highlight=get#cv2.VideoCapture.get
 # https://www.geeksforgeeks.org/python-assert-keyword/
 
-# Define the codec and create VideoWriter object
 fourcc = cv.VideoWriter_fourcc(*'mp4v')
 
 now = datetime.now()                        
