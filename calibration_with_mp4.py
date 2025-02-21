397c000872d26c7ccbbfb982dd7a3fba7047869b
Jimin Kim <0305jimin@gmail.com>
2025-02-21 21:47:20 +0900

    calibration_with_mp4: removing comments

--- calibration_with_mp4.py
+++ calibration_with_mp4.py
@@ -1,29 +1,25 @@
 import cv2 as cv
 import numpy as np
 
-# termination criteria
 criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
 W = 7
 L = 10 
 
-# prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
 objp = np.zeros((W*L, 3), np.float32)
 objp[:,:2] = np.mgrid[0:L, 0:W].T.reshape(-1, 2)
 
-# Arrays to store object points and image points from all the images.
-objpoints = [] # 3d point in real world space
-imgpoints = [] # 2d points in image plane.
+objpoints = []
+imgpoints = []
 
 videopath = 'input/2025-02-11-111507-h-3.mp4'
-cap = cv.VideoCapture(videopath) # capturing a video file. after capturing -> can read the frames one by one.
+cap = cv.VideoCapture(videopath)
 print(cap)
 # value of the constants: https://docs.opencv.org/4.3.0/d4/d15/group__videoio__flags__base.html ;  stored using enum. 
 
 idx = 0
 while (cap.isOpened()):
-    isframeavailable, frame = cap.read()  # read the next frame of the video. 
-    # print("Frame available exist:", ret)
+    isframeavailable, frame = cap.read()
     if isframeavailable != True:
         print("Fetching Frame ended in INDEX: ", idx)
         break
@@ -31,7 +27,6 @@ while (cap.isOpened()):
 
     grayscaledimg = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
         
-    # Find the chess board corners
     patternfound, corners = cv.findChessboardCorners(grayscaledimg, (L, W), None)
     if idx % 50 == 0: print(f"chesssboard pattern of {idx}th frame has been found:", patternfound) 
 
@@ -41,7 +36,6 @@ while (cap.isOpened()):
         corners2 = cv.cornerSubPix(grayscaledimg, corners, (11,11), (-1,-1), criteria)
         imgpoints.append(corners2)
     
-        # Draw and display the corners
         cv.drawChessboardCorners(frame, (L, W), corners2, patternfound)
 
 print("\nCalibrating the Camera matrix and distortion coefficient\n")
@@ -54,5 +48,5 @@ if iscalibrated:
 else:
     print("Calibration failed")
 
-cap.release() # closing a video file
+cap.release()
 
