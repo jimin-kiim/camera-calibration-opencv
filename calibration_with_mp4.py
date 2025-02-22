48ee15c5f4423029f4d00809af47ccb053476160
Jimin Kim <0305jimin@gmail.com>
2025-02-22 15:12:55 +0900

    calibration_with_*: renaming variables for better readability

--- calibration_with_mp4.py
+++ calibration_with_mp4.py
@@ -1,45 +1,47 @@
 import cv2 as cv
 import numpy as np
 
-criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+termiation_criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
-W = 7
-L = 10 
+chessboard_width = 7
+chessboard_length = 10 
 
-objp = np.zeros((W*L, 3), np.float32)
-objp[:,:2] = np.mgrid[0:L, 0:W].T.reshape(-1, 2)
+objp = np.zeros((chessboard_width * chessboard_length, 3), np.float32)
+objp[:,:2] = np.mgrid[0:chessboard_length, 0:chessboard_width].T.reshape(-1, 2)
 
-objpoints = []
+realworldpoints = []
 imgpoints = []
 
-videopath = 'input/2025-02-11-111507-h-3.mp4'
+videofilename = "2025-02-11-111507-h-3.mp4"
+videopath = f'input/{videofilename}'
 cap = cv.VideoCapture(videopath)
-print(cap)
-# value of the constants: https://docs.opencv.org/4.3.0/d4/d15/group__videoio__flags__base.html ;  stored using enum. 
+if not cap:
+    print("Error occured while instantiating Video Capture")
 
 idx = 0
 while (cap.isOpened()):
     isframeavailable, frame = cap.read()
+
     if isframeavailable != True:
         print("Fetching Frame ended in INDEX: ", idx)
         break
     idx += 1
 
     grayscaledimg = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-        
-    patternfound, corners = cv.findChessboardCorners(grayscaledimg, (L, W), None)
+    
+    patternfound, corners = cv.findChessboardCorners(grayscaledimg, (chessboard_length, chessboard_width), None)
     if idx % 50 == 0: print(f"chesssboard pattern of {idx}th frame has been found:", patternfound) 
 
     if patternfound:
-        objpoints.append(objp)
+        realworldpoints.append(objp)
     
-        corners2 = cv.cornerSubPix(grayscaledimg, corners, (11,11), (-1,-1), criteria)
+        corners2 = cv.cornerSubPix(grayscaledimg, corners, (11,11), (-1,-1), termiation_criteria)
         imgpoints.append(corners2)
-    
-        cv.drawChessboardCorners(frame, (L, W), corners2, patternfound)
+
+        cv.drawChessboardCorners(frame, (chessboard_length, chessboard_width), corners2, patternfound)
 
 print("\nCalibrating the Camera matrix and distortion coefficient\n")
-iscalibrated, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, grayscaledimg.shape[::-1], None, None)
+iscalibrated, mtx, dist, rvecs, tvecs = cv.calibrateCamera(realworldpoints, imgpoints, grayscaledimg.shape[::-1], None, None)
 
 if iscalibrated:
     print("Calibration is successfully done.")
@@ -49,4 +51,3 @@ else:
     print("Calibration failed")
 
 cap.release()
-
