da63a8837e8c378507b1b9c700e789a3c2300908
Jimin Kim <0305jimin@gmail.com>
2025-02-22 12:30:30 +0900

    calibration ipynb py: code tyding up

--- calibration.py
+++ calibration.py
@@ -2,6 +2,15 @@ import cv2 as cv
 import numpy as np
 import matplotlib.pyplot as plt
 
+def showimg(windowname, img, atthelast = False):
+    cv.imshow(windowname, img)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
+
+    if atthelast:
+        cv.waitKey(1000)
+
+
 criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
 W = 23
@@ -15,32 +24,27 @@ imgpoints = []
 
 imgname = 'camera_calib5.jpg'
 imgpath = f'images/{imgname}'
+
+
 img = cv.imread(imgpath)
-cv.imshow(imgname, img)
-cv.waitKey(0)
-cv.destroyAllWindows()
+showimg(imgname, img)
 
 grayscaledimg = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 grayscaledimgtitle = imgname + " grayscaled"
-cv.imshow(grayscaledimgtitle, grayscaledimg)
-cv.waitKey(0)
-cv.destroyAllWindows()
+showimg(grayscaledimgtitle, grayscaledimg)
 
 ret, corners = cv.findChessboardCorners(grayscaledimg, (L,W), None)
 print("Are Chessboard corners detected:\n", ret, "\n")
 
-if ret == True:
+if ret:
     objpoints.append(objp)
 
     corners2 = cv.cornerSubPix(grayscaledimg, corners, (11, 11), (-1, -1), criteria)
     imgpoints.append(corners2)
 
-    # Draw and display the corners
     cv.drawChessboardCorners(img, (L, W), corners2, ret)
     window_name = imgpath + ' with corner'
-    cv.imshow(window_name,img)
-    cv.waitKey(0)
-    cv.destroyAllWindows()
+    showimg(window_name, img)
 
     ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, grayscaledimg.shape[::-1], None, None)
     print(ret)
@@ -56,8 +60,4 @@ if ret == True:
     x, y, w, h = roi
     dst = dst[y:y+h, x:x+w]
     outputpath = f"output/{imgname}"
-    cv.imshow(window_name, dst)
-    cv.waitKey(0)
-
-    cv.destroyAllWindows()
-    cv.waitKey(1000)
+    showimg(window_name, dst, True)
